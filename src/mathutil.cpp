fe0e27d23c94dc1922bf2064ec1aece9eb5f2334
Adam Higerd <chighland@gmail.com>
2024-11-05 17:36:09 -0600

    unused (for now) expandPolygon function

--- src/mathutil.cpp
+++ src/mathutil.cpp
@@ -42,3 +42,57 @@ double ccwAngle(const QPointF& a, const QPointF& b, const QPointF& c)
   double t = std::atan2(y2 * x1 - x2 * y1, x1 * x2 + y1 * y2);
   return (t < 0) ? t + M_PI * 2 : t;
 }
+
+// Assumes counterclockwise winding angles
+QPainterPath expandPolygon(const QPolygonF& source, double distance)
+{
+  QPainterPath result;
+
+  int numVertices = source.length();
+  if (numVertices < 1) {
+    return result;
+  } else if (numVertices == 1) {
+    QPointF p = source[0];
+    result.addEllipse(p.x() - distance, p.y() - distance, distance * 2, distance * 2);
+    return result;
+  }
+
+  QPointF p1 = source[source.length() - 1];
+  QLineF norm(0, 0, 0, 0);
+  QLineF lastVector, vector;
+  QPointF nextPoint;
+
+  bool first = true;
+  for (int i = 0; i <= numVertices; i++) {
+    QPointF p2 = source[i % numVertices];
+    norm.setP2(QPointF(p1.y() - p2.y(), p2.x() - p1.x()));
+    norm.setLength(distance);
+    if (!norm.length()) {
+      continue;
+    }
+    QPointF n = norm.p2();
+    vector.setPoints(p1 - n, p2 - n);
+    if (first) {
+      result.moveTo((p1 + p2) / 2 - n);
+      first = false;
+    } else {
+      QPointF control;
+      auto intersect = lastVector.intersects(vector, &control);
+      if (intersect == QLineF::NoIntersection) {
+        continue;
+      } else if (intersect == QLineF::BoundedIntersection) {
+        result.lineTo(control);
+      } else {
+        result.lineTo(nextPoint);
+        result.cubicTo((lastVector.p2() + control) / 2, (p1 - n + control) / 2, p1 - n);
+      }
+    }
+    nextPoint = p2 - n;
+
+    lastVector = vector;
+    p1 = p2;
+  }
+  result.closeSubpath();
+
+  return result;
+}
